3772ce610152422e938273dccc5556f3af3f7f26
Vineeth <vineeth.t.mukundan@gmail.com>
2019-05-20 09:17:16 +0000

    second_commit

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,48 @@
 import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df =pd.merge(messages,categories,on='id')
+    
+    return df
+    
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(';',expand=True)
+    one_row=categories.loc[0]
+    category_colnames = one_row.apply(lambda x:x.split('-')[0])
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x:x.split('-')[1])
 
-
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+        
+    for col in categories.columns:
+        if len(categories[col].unique())==1:
+            categories.drop(col,axis=1,inplace=True)
+        elif len(categories[col].unique())>2:
+                categories[col]=categories[col].apply(lambda x: 1 if x !=0 else 0) 
+                
+    df.drop(['categories'],axis=1,inplace=True)
+    df2 = pd.concat([df,categories],axis=1)
+    df3=df2.drop_duplicates(keep='first')
+    
+    return df3
+    
+    
+    
+    
+        
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('cleaned_messages', engine, index=False)
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,16 +1,83 @@
 import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 
+
+import nltk
+nltk.download(['punkt', 'wordnet','stopwords'])
+
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer,TfidfVectorizer
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
+from sklearn.base import BaseEstimator, TransformerMixin
 
 def load_data(database_filepath):
-    pass
+    engine=create_engine('sqlite:///'+file_path)
+    df=pd.read_sql_table('cleaned_messages',con=engine)  
+    X=df['message']
+    y=df.iloc[:,4::]
+    return X,y
 
 
 def tokenize(text):
-    pass
+    text=re.sub(r"[^a-zA-Z0-9]"," ",text)
+    tokens=word_tokenize(text)   
+    stop_words=stopwords.words('english') 
+    tokens=[word for word in tokens if word not in stop_words]
+    lemmatizer= WordNetLemmatizer()    
+    clean_tokens=[]   
+    for tok in tokens:
+        clean_tok=lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)    
+    return clean_tokens
+
+class array_transformer(BaseEstimator, TransformerMixin):
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_array = X.toarray()
+        return X_array
+    
+f1_scorer=make_scorer(f1_score,average='weighted')
 
 
 def build_model():
-    pass
+    pipeline=Pipeline([('tfidfvect',TfidfVectorizer(tokenizer=tokenize)),
+                   ('toarray',array_transformer()),
+                  ('scaler',StandardScaler()),('pca',PCA()),
+                  ('clf',MultiOutputClassifier(estimator=DecisionTreeClassifier()))])
+    
+    parameters= {
+        'tfidfvect__max_features':[5000],
+        'pca__n_components':[100,200] ,
+    'clf__estimator__max_depth':[5,10]}
+    cv=GridSearchCV(pipeline,param_grid=parameters,scoring=f1_scorer,cv=5)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
